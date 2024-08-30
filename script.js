1cbf60509bd48eaa08b66a00058fe6d0c925216f
Vikash upadhyay <109405332+vku42@users.noreply.github.com>
2024-08-30 22:55:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const textBox = document.getElementById("text-box");
 
 // Dark or Light Images
 function imageMode(color) {
-  image1.src = `./img/undraw_proud_coder_${color}.svg`;
-  image2.src = `./img/undraw_feeling_proud_${color}.svg`;
-  image3.src = `./img/undraw_conceptual_idea_${color}.svg`;
+  image1.src = `undraw_proud_coder_${color}.svg`;
+  image2.src = `undraw_feeling_proud_${color}.svg`;
+  image3.src = `undraw_conceptual_idea_${color}.svg`;
 }
 
 function toggleDarkLightMode(isLight) {
